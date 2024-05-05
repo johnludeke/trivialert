c7fe30447565fdbd98252b01917ad69e95dc01d2
John Ludeke <114034635+johnludeke@users.noreply.github.com>
2024-05-05 09:54:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,46 +3,35 @@ import praw
 from time import sleep
 from slack_bolt import App
 
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-
 # Initialize your app with your bot token and signing secret
 app = App(
     token = os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret = os.environ.get("SLACK_SIGNING_SECRET"),
 )
 
-# for testing : C06RQBBAL0P
-# for trivia  : C06L12LFC0G
-channel_id = "C06L12LFC0G"
+channel_id = "YOUR_CHANNEL_ID_HERE"
 
 # Initialize the Reddit API client
 reddit = praw.Reddit(
     client_id = os.environ.get("REDDIT_CLIENT_ID"),
     client_secret = os.environ.get("REDDIT_CLIENT_SECRET"),
-    user_agent = "trivialert",
+    user_agent = "NAME_OF_YOUR_BOT",
 )
 
-# Specify the Reddit username to monitor
-username = "Murphyspubuofi"
+username = "REDDIT_USERNAME_TO_TRACK"
 
-# Keep track of the latest post ID
 latest_post_id = None
 
-# Function to handle new posts by the specified user
 def handle_new_post(post):
     global latest_post_id
 
-    # Extract relevant information from the post
     title = post.title
     url = post.url
     text = post.selftext
     subreddit = post.subreddit.display_name
 
-    # Compose the message to be sent to Slack
-    message = f":alert: HEY <!channel> :alert:\n{username} in r/{subreddit} just posted :blobhyper:\n\n*{title}*\n{text}\n\n({url})"
+    message = "YOUR_FORMATTED_MESSAGE"
 
-    # Send the message to the specified Slack channel
     app.client.chat_postMessage(channel=channel_id, text=message)
 
     latest_post_id = post.id
@@ -65,10 +54,8 @@ while True:
                 sleep(10)
                 break  # Exit the loop after processing the first post
     except Exception as e:
-        # Handle exceptions gracefully (e.g., rate limit exceeded)
         print(f"Error occurred: {e}")
     sleep(10)
 
-# Launch app
 if __name__ == "__main__":
-    app.start(port = int(os.environ.get("PORT", 3000)))
+    app.start(port = int(os.environ.get("PORT", YOUR_DESIRED_PORT)))
